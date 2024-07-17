1aa8fb003f72150c190a457baa55e813fcff1b8c
matsufriends <pikapikan0526@gmail.com>
2024-07-18 00:14:07 +0900

    init

---
+++ MornEditorDynamicFontCleaner.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace MornEditor
+{
+    [InitializeOnLoad]
+    public static class MornEditorDynamicFontCleaner
+    {
+        static MornEditorDynamicFontCleaner()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
+                var tmpFontAssets = Resources.FindObjectsOfTypeAll<TMP_FontAsset>();
+                foreach (var tmpFontAsset in tmpFontAssets)
+                {
+                    if (tmpFontAsset != null && tmpFontAsset.atlasPopulationMode == AtlasPopulationMode.Dynamic)
+                    {
+                        tmpFontAsset.ClearFontAssetData();
+                        Debug.Log($"[{nameof(MornEditorDynamicFontCleaner)}]:ClearFontAssetData\"{tmpFontAsset.name}\"");
+                    }
+                }
+            }
+        }
+    }
+}
