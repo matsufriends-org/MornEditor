58c0d32ea13470a3156ba7e289746d08eea5fe30
matsufriends <pikapikan0526@gmail.com>
2025-02-05 03:36:38 +0900

    RenderStaticPreviewの追加

--- MornEditorUtil.cs
+++ MornEditorUtil.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Reflection;
 using UnityEngine;
+using Object = UnityEngine.Object;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -53,5 +56,49 @@ namespace MornEditor
             LogWarning("PrefabUtility.InstantiatePrefab is only available in Editor.");
             return Object.Instantiate(prefab, parent);
         }
+
+#if UNITY_EDITOR
+        public static Texture2D RenderStaticPreview(Sprite sprite, Editor editor, string assetPath, Object[] subAssets, int width, int height)
+        {
+            if (sprite != null)
+            {
+                var t = GetType("UnityEditor.SpriteUtility");
+                if (t != null)
+                {
+                    var method = t.GetMethod("RenderStaticPreview", new[] { typeof(Sprite), typeof(Color), typeof(int), typeof(int) });
+                    if (method != null)
+                    {
+                        var ret = method.Invoke("RenderStaticPreview", new object[] { sprite, Color.white, width, height });
+                        if (ret is Texture2D)
+                        {
+                            return ret as Texture2D;
+                        }
+                    }
+                }
+            }
+            return editor.RenderStaticPreview(assetPath, subAssets, width, height);
+        }
+
+        private static Type GetType(string typeName)
+        {
+            var type = Type.GetType(typeName);
+            if (type != null)
+                return type;
+
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var referencedAssemblies = currentAssembly.GetReferencedAssemblies();
+            foreach (var assemblyName in referencedAssemblies)
+            {
+                var assembly = Assembly.Load(assemblyName);
+                if (assembly != null)
+                {
+                    type = assembly.GetType(typeName);
+                    if (type != null)
+                        return type;
+                }
+            }
+            return null;
+        }
+#endif
     }
 }
